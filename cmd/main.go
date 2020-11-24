0a2b12b61011c533bdf8d2cf6b9df8e6042d999a
yokoyamada <56547256+yokoyamada@users.noreply.github.com>
2020-11-24 10:35:23 +0900

    add gin and gorm

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+)
 
+func gormConnect() *gorm.DB {
+	DBMS := "mysql"
+	USER := "root"
+	PASS := "password"
+	PROTOCOL := "tcp(localhost:3306)"
+	DBNAME := "tododb"
+	CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME
+
+	db, err := gorm.Open(DBMS, CONNECT)
+	if err != nil {
+		panic(err.Error())
+	}
+	return db
+}
+
+func main() {
+	engine := gin.Default()
+	engine.LoadHTMLGlob("asset/html/*.html")
+	engine.Static("/asset/css", "asset/css")
+
+	engine.GET("/", func(c *gin.Context) {
+
+	})
 }
