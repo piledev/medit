242f4ff73b7935f99b430a2643a9b4334f652314
yokoyamada <pile.dev@gmail.com>
2020-12-07 10:35:25 +0900

    introduce react-calendar-heatmap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,59 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import CalendarHeatmap from 'react-calendar-heatmap';
+import ReactTooltip from 'react-tooltip';
+import 'react-calendar-heatmap/dist/styles.css';
+// import './styles.css';
+
+const today = new Date();
 
 function App() {
+  const randomValues = getRange(365).map(index => {
+    return {
+      date: shiftDate(today, -index),
+      count: getRandomInt(1, 3),
+    };
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>medit.</h1>
+      <p>meditation recorder for meditation lovers.</p>
+      <CalendarHeatmap
+        startDate={shiftDate(today, -365)}
+        endDate={today}
+        values={randomValues}
+        classForValue={value => {
+          if (!value) {
+            return 'color-empty';
+          }
+          return `color-github-${value.count}`;
+        }}
+        tooltipDataAttrs={(value: any) => {
+          return {
+            'data-tip': `${value.date.toISOString().slice(0, 10)} has count: ${
+              value.count
+            }`,
+          };
+        }}
+        showWeekdayLabels={true}
+        onClick={value => alert(`Clicked on value with count: ${value.count}`)}
+      />
+      <ReactTooltip />
     </div>
   );
+}
+
+function shiftDate(date: Date, numDays: number) {
+  const newDate = new Date(date);
+  newDate.setDate(newDate.getDate() + numDays);
+  return newDate;
+}
+
+function getRange(count: number) {
+  return Array.from({ length: count },(_,i) => i);
+}
+
+function getRandomInt(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 export default App;
