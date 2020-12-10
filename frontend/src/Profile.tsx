3f3a2ac454df2bac5d49c988344493e87481ddcf
yokoyamada <pile.dev@gmail.com>
2020-12-10 09:45:13 +0900

    in progress

--- frontend/src/Profile.tsx
+++ frontend/src/Profile.tsx
@@ -1,13 +1,12 @@
-import React from 'react'
-
-// プロフィール写真(Gravatarから持ってきたい)
-// ID(登録名)
-// Rank
+//import React from 'react'
+import { Avatar } from '@material-ui/core';
+import './Profile.css'
 
 function Profile() {
     return (
         <div className="profile">
-            <img src="../public/meditation.jpg" alt="gravatar"/>
+            {/* <img className="avatar" src="meditation.jpg" alt="gravatar"/> */}
+            <Avatar src="meditation.jpg" alt="avatar" />
             <h1>a meditator</h1>
             <h2>Silver Meditator</h2>
         </div>
